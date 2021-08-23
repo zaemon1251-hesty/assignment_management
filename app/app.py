c91a15544824ceda0ffd77ad186d852016f2191f
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-08-24 00:29:14 +0900

    ファイル名変更 & 課題 検索機能修正

--- app/app.py
+++ app/app.py
@@ -1,27 +1,15 @@
 from flask import Flask, request, render_template, redirect, url_for
-import servise
+import service
 # config
 status = {0:"end",1:"normal",2:"semi",3:"hot"}
 app = Flask(__name__)
-
-
-def request_user_id(request):
-    try:
-        user_id = int(request.args.get('user_id', -1))
-    except:
-        user_id = -1
-    if user_id == -1:
-        user_id = None
-    return user_id
-
-
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     user_id = request_user_id(request)
     print("user_id got: %s" % user_id)
-    user = servise.get_user(id=user_id)
+    user = service.get_user(id=user_id)
     print("got user///")
     print(user)
     if request.method == 'POST':
@@ -30,9 +18,9 @@ def index():
         conditions['course'] = "%{}%".format(request.form['course']) if request.form['course'] != '' else None
         conditions['assignment'] = "%{}%".format(request.form['assignment']) if request.form['assignment'] != '' else None
         conditions['info'] = "%{}%".format(request.form['info']) if request.form['info'] != '' else None
-        datas = servise.get(state=state, user_id=user_id, conditions=conditions)
+        datas = service.get(state=state, user_id=user_id, conditions=conditions)
     else:
-        datas = servise.get(user_id=user_id)
+        datas = service.get(user_id=user_id)
     return render_template('index.html', datas=datas, user=user, status=status)
 
 
@@ -43,7 +31,7 @@ def add():
         keywords = request.form['keywords'].split()
     else:
         keywords = []
-    no_problem = servise.add(user_id=user_id, keywords=keywords)
+    no_problem = service.add(user_id=user_id, keywords=keywords)
     return redirect(url_for('index', user_id=user_id))
 
 
@@ -54,7 +42,7 @@ def change(id, state):
         user_id = None
     print("user_id got: %s" % user_id)
     if request.method == 'GET':
-        no_problem = servise.changeStatus(state=state, id=id, user_id=user_id)
+        no_problem = service.changeStatus(state=state, id=id, user_id=user_id)
     return redirect(url_for('index', user_id=user_id))
 
 
@@ -62,7 +50,7 @@ def change(id, state):
 def login():
     # id passwd でのログイン処理などの実装もできたらしたい
     name = request.args.get('name')
-    user  =servise.get_user(name=name)
+    user  =service.get_user(name=name)
     if user:
         user_id=user.id
     else:
@@ -77,12 +65,21 @@ def user():
         name = request.form['name']
         userid = request.form['moodle_userid']
         passwd = request.form['moodle_passwd']
-        user_id = servise.add_user(name, userid, passwd)
+        user_id = service.add_user(name, userid, passwd)
         return redirect(url_for('index', user_id=user_id))
     else:
-        user = servise.get_user(id=user_id)
+        user = service.get_user(id=user_id)
         return render_template('user.html', user=user)
 
+
+def request_user_id(request):
+    try:
+        user_id = int(request.args.get('user_id', -1))
+    except:
+        user_id = -1
+    if user_id == -1:
+        user_id = None
+    return user_id
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888, debug=True)
