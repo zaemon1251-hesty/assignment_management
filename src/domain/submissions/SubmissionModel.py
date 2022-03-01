647495daa3e95fa3743ef75f89ba45e097b02fac
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-03-02 03:15:28 +0900

    fix  domain/Submission

--- src/domain/submissions/SubmissionModel.py
+++ src/domain/submissions/SubmissionModel.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from src.domain.assignments.AssignmentModel import Assignment
 from src.domain.submissions.SubmissionState import SUBMISSION_STATE, submission_state
-from src.domain.users.UserModel import user
+from src.domain.users.UserModel import User
 
 
 @dataclass(init=False, eq=True, frozen=True)
@@ -13,14 +13,14 @@ class Submission:
     def __init__(
         self,
         id: str,
-        user: user,
+        user: User,
         assignment: Assignment,
         state: Optional[submission_state] = SUBMISSION_STATE.NORMAL,
         created_at: Optional[int] = None,
         updated_at: Optional[int] = None,
     ):
         self.id: str = id
-        user: user = user,
+        user: User = user,
         assignment: Assignment = assignment,
         self.state: submission_state = state
         self.created_at: Optional[int] = created_at
