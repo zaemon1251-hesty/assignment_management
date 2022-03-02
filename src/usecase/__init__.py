75ef5d88a2efb2a464683147eeb2ffc108cbc33f
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-03-02 21:43:06 +0900

    構成の変更

--- src/usecase/__init__.py
+++ src/usecase/__init__.py
@@ -1,15 +1,4 @@
-from enum import Enum
-from typing import Union
-
-from src.domain.users.UserModel import User
 from src.domain.courses.CourseModel import Course
+from src.domain.users.UserModel import User
 from src.domain.assignments.AssignmentModel import Assignment
 from src.domain.submissions.SubmissionModel import Submission
-
-
-DOMAINS = Union[
-    User,
-    Course,
-    Assignment,
-    Submission,
-]
