fefdcb7f40605cf78f789563f2a13bdfdb569069
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-03-03 00:31:43 +0900

    infrastractures init

---
+++ src/infrastructure/postgresql/schedulers/SchedulerOrm.py
@@ -0,0 +1,19 @@
+from pydantic.dataclasses import dataclass
+from pydantic import BaseModel
+from typing import Optional
+
+from src.domain.submissions.SubmissionModel import Submission
+
+
+class Scheduler(BaseModel):
+    """Scheduler represents what assignment should be reminded to whom at a certain time """
+
+    id: int
+    submission: Submission
+    remind_at: int
+    reminded: Optional[bool] = False
+    created_at: Optional[int] = None
+    updated_at: Optional[int] = None
+
+    class Config:
+        orm_mode = True
