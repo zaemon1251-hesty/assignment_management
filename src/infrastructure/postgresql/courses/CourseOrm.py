55f9da70787305da554aff2aa16fdf80ebb585db
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-03-04 03:41:56 +0900

    feat orm

--- src/infrastructure/postgresql/courses/CourseOrm.py
+++ src/infrastructure/postgresql/courses/CourseOrm.py
@@ -1,16 +1,47 @@
+from datetime import datetime, timezone
 from pydantic.dataclasses import dataclass
 from pydantic import BaseModel
 from typing import Optional
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from src.domain.courses.CourseModel import Course
+from src.infrastructure.postgresql.database import Base
 
 
-class Course(BaseModel):
-    """course represents your collection of course as an entity."""
+class CourseOrm(Base):
+    """
+    Courses (model)
+    params:
+    id->int
+    title->string
+    """
+    __tablename__ = 'courses'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(200))
+    url = Column(String(200))
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime)
 
-    id: int
-    title: str
-    url: str
-    created_at: Optional[int] = None
-    updated_at: Optional[int] = None
+    assignments = relationship("Assignments", backref="courses")
 
-    class Config:
-        orm_mode = True
+    @classmethod
+    def to_domain(self) -> Course:
+        return Course(
+            id=self.id,
+            title=self.title,
+            url=self.url,
+            created_at=self.created_at,
+            updated_at=self.updated_at
+        )
+
+    @staticmethod
+    def from_domain(data: Course) -> "CourseOrm":
+        now = datetime.now(timezone.utc)
+        return CourseOrm(
+            id=data.id,
+            title=data.title,
+            url=data.url,
+            created_at=now,
+            updated_at=now
+        )
--- src/infrastructure/postgresql/submissions/SubmissionOrm.py
+++ src/infrastructure/postgresql/submissions/SubmissionOrm.py
@@ -1,38 +1,72 @@
-from pydantic import BaseModel
-from pydantic.dataclasses import dataclass
+from datetime import datetime
+from datetime import timezone
 from typing import Optional
-
+import sys
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from src.infrastructure.postgresql.database import Base
 from src.domain.assignments.AssignmentModel import Assignment
+from src.domain.users.UserModel import User
 from src.domain.assignments.AssignmentState import ASSIGNMENT_STATE
-from src.domain.submissions.SubmissionState import SUBMISSION_STATE, submission_state
+from src.domain.submissions.SubmissionState import SUBMISSION_STATE
+from src.domain.submissions.SubmissionModel import Submission
 from src.domain.users.UserModel import User
 
 
-class Submission(BaseModel):
-    """submission represents your registerd assignment as an entity."""
+class SubmissionOrm(Base):
+    """
+    Submissions (model)
+    params:
+    id->int
+    user_id->int
+    assignment_id->int
+    state->int
+    """
+    __tablename__ = 'submissions'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(
+        ForeignKey(
+            'users.id',
+            onupdate='CASCADE',
+            ondelete='CASCADE'
+        ),
+        nullable=False
+    )
+    assignment_id = Column(
+        ForeignKey(
+            'assignments.id',
+            onupdate='CASCADE',
+            ondelete='CASCADE',
+            nullable=False
+        )
+    )
+    state = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime)
 
-    id: int
-    user: User
-    assignment: Assignment
-    state: Optional[submission_state] = SUBMISSION_STATE.NORMAL
-    created_at: Optional[int] = None
-    updated_at: Optional[int] = None
+    @classmethod
+    def to_domain(self) -> Submission:
+        """almost same as Domain.from_orm() """
+        _user = User.from_orm(self.user)
+        _assignment = Assignment.from_orm(self.assignment)
+        return Submission(
+            id=self.id,
+            user=_user,
+            assignment=_assignment,
+            state=SUBMISSION_STATE(self.state),
+            created_at=self.created_at,
+            updated_at=self.updated_at
+        )
 
-    class Config:
-        orm_mode = True
-
-    def is_already_expired(self) -> bool:
-        """ this condition is not expected
-            system should prevent this in usecase layer
-        """
-        return (
-            self.state == SUBMISSION_STATE.NORMAL or
-            self.state == SUBMISSION_STATE.DANGER
-        ) and self.assignment.state == ASSIGNMENT_STATE.DEAD
-
-    def is_still_alive(self) -> bool:
-        """ this condition is not expected
-            system should prevent this in usecase layer
-        """
-        return self.state == SUBMISSION_STATE.EXPIRED and \
-            self.assignment.state == ASSIGNMENT_STATE.ALIVE
+    @staticmethod
+    def from_domain(data: Submission) -> "SubmissionOrm":
+        now = datetime.now(timezone.utc)
+        return SubmissionOrm(
+            id=data.id,
+            user_id=data.user.id,
+            assignment_id=data.assignment.id,
+            state=int(data.state),
+            created_at=now,
+            updated_at=now,
+        )
