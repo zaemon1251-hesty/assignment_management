1bc9d5c020561b863fc89c5db2616f528b0d897a
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-03-08 20:18:26 +0900

    IRepository move to domain

--- src/interface/repository/__init__.py
+++
@@ -1,5 +0,0 @@
-# interface adapter に属する repository で
-# domain を引数に持つメソッドを定義するのは、依存性の逆流を引き起こす（usecaseもだが、その層はビジネスロジックだから自分ルールではセーフ）
-# (domain)ReadModelなり、(domain)CommandModel という緩衝材を挟むか、
-# 原始的な型の可変長引数で実装するか
-# 一旦はそのままでいいか
