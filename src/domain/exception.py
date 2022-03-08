f63c76b25b341a14c70d5cff9b2309d5de2926af
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-03-08 20:23:21 +0900

    forgat to commit (domain)

--- src/domain/exception.py
+++ src/domain/exception.py
@@ -4,7 +4,18 @@ from src.domain import DOMAINS
 
 
 class TargetNotFoundException(Exception):
-    """ターゲットが存在しない時の例外"""
+    """存在するはずのターゲットが存在しない際の例外"""
+    domain: DOMAINS
+
+    def __init__(self, message, domain: DOMAINS, *args, **kwargs):
+        self.domain = domain
+        message += "\n"
+        message += str(domain)
+        super().__init__(message)
+
+
+class TargetAlreadyExsitException(Exception):
+    """存在しないはずのターゲットが存在する際の例外"""
     domain: DOMAINS
 
     def __init__(self, message, domain: DOMAINS, *args, **kwargs):
@@ -15,5 +26,15 @@ class TargetNotFoundException(Exception):
 
 
 class StateContradictedException(Exception):
-    "課題の状態と、提出物の状態が矛盾するときの例外"
+    """課題の状態と、提出物の状態が矛盾するときの例外"""
+    pass
+
+
+class UnauthorizedException(Exception):
+    """ユーザー認証失敗時の例外"""
+    pass
+
+
+class CredentialsException(Exception):
+    """tokenを検証出来ない時の例外"""
     pass
