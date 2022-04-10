753035d96c727cc92ab418c6dac4f0d062e768e2
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-04-10 12:48:09 +0900

    refactoring scrape driver

--- src/infrastructure/crawl/scraping.py
+++ src/infrastructure/crawl/scraping.py
@@ -1,12 +1,12 @@
+from ctypes import Union
 from datetime import datetime
 import os
 import time
 import re
 import traceback
 from typing import Any, Dict, List, Optional, Tuple
+from unicodedata import category
 from urllib import parse
-from uuid import uuid4
-from bs4 import BeautifulSoup, ResultSet
 from pydantic import BaseModel, ValidationError
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
@@ -103,12 +103,17 @@ class ScrapeDriverImpl(ScrapeDriver):
     async def run(self, keywords: List[str] = []) -> Tuple[List[Assignment], List[Course]]:
         assignments: List[Assignment] = []
         courses: List[Course] = []
-        moodle_id = os.getenv("MOODLE_ID")
-        moodle_password = os.getenv("MOODLE_PASSWORD")
 
         print("START SCRAPING")
-        curde_assis, crude_cors = await self.get_assignments(moodle_id, moodle_password, keywords)
-        print(crude_cors)
+
+        t = await self.login_moodle()
+        t = await self.move_to_mypage()
+        t = await self.show_all_courses()
+        crude_cors = await self._run_courses(keywords)
+        curde_assis = await self._run_assignments()
+
+        self.driver.quit()
+
         print("END SCRAPING")
 
         try:
@@ -118,9 +123,6 @@ class ScrapeDriverImpl(ScrapeDriver):
                 courses.append(self.parse_course(key, crude))
         except ValidationError as e:
             traceback.print_exc()
-        # except Exception as e:
-        #     logger.error(e)
-        print(assignments, courses)
         return assignments, courses
 
     @classmethod
@@ -145,30 +147,9 @@ class ScrapeDriverImpl(ScrapeDriver):
         )
         return assign
 
-    async def login(self, url: str, mid: str, passwd: str):
-        pass
-
-    async def _extract_courses(self):
-        pass
-
-    async def _extract_assignments(self):
-        pass
-
-    def _get_category(self, element: Optional[WebElement]):
-        pass
-
-    async def get_assignments(self, userid: str, passwd: str, keywords: List[str] = None) -> Tuple[Dict[int, crude_assignment], Dict[int, crude_course]]:
-        """
-        moodleのページからコースと課題を取ってくる
-        params:
-        userid -> int
-        passwd -> int
-        keywords -> list(string)
-        keywords: ゲットしたい課題のタグの条件
-        """
-        # 保存するデータを格納する
-        lectures: Dict[int, crude_course] = {}
-        assignments: Dict[int, crude_assignment] = {}
+    async def login_moodle(self):
+        userid = os.getenv("MOODLE_ID")
+        passwd = os.getenv("MOODLE_PASSWORD")
 
         # moodleにログイン
         if not isinstance(userid, str) or not isinstance(passwd, str):
@@ -192,7 +173,9 @@ class ScrapeDriverImpl(ScrapeDriver):
             raise
         print("Logined in moodle!")
         print("current URL: {}".format(self.driver.current_url))
+        return True
 
+    async def move_to_mypage(self) -> None:
         # ログイン後遷移
         self.driver.get(TARGET_URL)
         try:
@@ -206,7 +189,8 @@ class ScrapeDriverImpl(ScrapeDriver):
             .until(EC.presence_of_all_elements_located)
         time.sleep(2)
 
-        # 全てのコースを表示する
+    async def show_all_courses(self) -> None:
+        """全てのコースを表示する"""
         try:
             print("current URL: {}".format(self.driver.current_url))
             self.driver.find_element_by_css_selector(
@@ -222,39 +206,114 @@ class ScrapeDriverImpl(ScrapeDriver):
             .until(EC.presence_of_all_elements_located)
         time.sleep(5)
 
+    async def _extract_courses(self, lecture: Optional[WebElement], defalut_num=0) -> bool:
+        """遷移予定の講義ページに移動"""
+        try:
+            course_href = lecture.find_elements_by_tag_name(
+                "a")[0].get_attribute("href")
+            lec_id = dict(
+                parse.parse_qsl(
+                    parse.urlsplit(course_href).query)).get("id", defalut_num)
+            self.courses_buffer.append((lec_id, course_href))
+            return True
+        except Exception:
+            print("COULDN'T GET LINK")
+            return False
+
+    async def _extract_assignments(self, assign: Optional[WebElement], lec_id=-1, deufault_num=-1):
+        """遷移予定の課題ページを保存"""
+        try:
+            assign_href = assign.find_elements_by_tag_name(
+                "a")[0].get_attribute("href")
+            assignment_id = dict(
+                parse.parse_qsl(
+                    parse.urlsplit(assign_href).query)).get('id', deufault_num)
+            self.assiginments_buffer.append(
+                (lec_id, assignment_id, assign_href))
+
+        except Exception:
+            print("COUDN'T GET ASSIGNMENT LINK")
+
+    def _get_category(
+            self, element: Optional[WebElement], keywords: List[str] = None) -> Union[str, None]:
+        """カテゴリを抽出する"""
+        category = ''
+        try:
+            category = element.find_elements_by_css_selector(
+                CategorySelector)[0].text
+        except Exception:
+            pass
+
+        # カテゴリ（年別や期間など）で条件に合うものだけを探す
+        conditions = True  # any(key in category for key in keywords)
+        if not conditions:
+            print(
+                "this category:{} doesn't match the keywords:{}".format(
+                    category, keywords))
+            return None
+        return category
+
+    async def _insert_crude_course(self, course_href, lec_id, lectures):
+        """講義ページからタイトルなどをスクレイピングして講義情報を作成する
+        その後、配列に格納する"""
+        lec_title = self.driver.find_element(
+            By.TAG_NAME, "h1").text
+        print("コース名: %s" % lec_title)
+        print("current URL: {}".format(self.driver.current_url))
+
+        # コースの保存
+        try:
+            lectures[int(lec_id)] = crude_course(
+                course_title=lec_title,
+                course_url=course_href
+            )
+            print("A COURSE GOT")
+        except Exception as e:
+            traceback.print_exc()
+            print("A COURSE MISSED", [lec_id, lec_title, course_href])
+
+    async def _insert_crude_assignment(self, assign_href, assignment_id, lec_id, assignments):
+        """課題ページからタイトルなどをスクレイピングして課題情報を作成する
+        その後、配列に格納する"""
+        # コース名と課題名をゲットする
+        assign_title = self.driver.find_element(
+            By.TAG_NAME, "h2").text
+        print("課題名: %s" % assign_title)
+        WebDriverWait(self.driver, 10)
+        # 情報をゲットする
+        info = ''
+        try:
+            info = self.driver.find_element_by_css_selector(
+                "td.cell,td.c1,td.lastcol").text
+        except Exception:
+            pass
+
+        # 課題の保存
+        try:
+            assignments[int(assignment_id)] = crude_assignment(
+                course_id=int(lec_id),
+                assignment_title=assign_title,
+                info=info,
+                url=assign_href,
+            )
+            print("AN ASSIGNMENT GOT")
+        except Exception:
+            print("AN ASSIGNMENT MISSED")
+
+    async def _run_courses(self, keywords) -> Dict[int, crude_course]:
         # 全コース取得
         courses: List[WebElement] = self.driver.find_elements_by_css_selector(
             CourseSelector)
         print("全コース数: %s" % len(courses))
 
         for i, lecture in enumerate(courses):
-            # カテゴリを抽出する
-            category = ''
-            try:
-                category = lecture.find_elements_by_css_selector(
-                    CategorySelector)[0].text
-            except Exception:
-                pass
 
-            # カテゴリ（年別や期間など）で条件に合うものだけを探す
-            conditions = True  # any(key in category for key in keywords)
-            if not conditions:
-                print(
-                    "this category:{} doesn't match the keywords:{}".format(
-                        category, keywords))
+            if not self._get_category(lecture, keywords=keywords):
+                continue
+            if not self._extract_courses(lecture=lecture, defalut_num=i):
                 continue
 
-            # ターゲットの講義の課題情報を取り出す
-            try:
-                course_href = lecture.find_elements_by_tag_name(
-                    "a")[0].get_attribute("href")
-                lec_id = dict(
-                    parse.parse_qsl(
-                        parse.urlsplit(course_href).query)).get("id", i)
-                self.courses_buffer.append((lec_id, course_href))
-            except Exception:
-                print("COULDN'T GET LINK")
-                continue
+        lectures: Dict[int, crude_course] = {}
 
         while (self.courses_buffer):
             lec_id, course_href = self.courses_buffer.pop()
@@ -262,21 +321,8 @@ class ScrapeDriverImpl(ScrapeDriver):
             # コース情報ページへ遷移
             self.driver.get(course_href)
             time.sleep(5)
-            lec_title = self.driver.find_element(
-                By.TAG_NAME, "h1").text
-            print("コース名: %s" % lec_title)
-            print("current URL: {}".format(self.driver.current_url))
 
-            # コースの保存
-            try:
-                lectures[int(lec_id)] = crude_course(
-                    course_title=lec_title,
-                    course_url=course_href
-                )
-                print("A COURSE GOT")
-            except Exception as e:
-                traceback.print_exc()
-                print("A COURSE MISSED", [lec_id, lec_title, course_href])
+            await self._insert_crude_course(course_href, lec_id, lectures)
 
             # コースにひもつく課題の取得
             assigns: List[WebElement] = self.driver.find_elements_by_css_selector(
@@ -285,54 +331,24 @@ class ScrapeDriverImpl(ScrapeDriver):
 
             # 課題のスクレイピング
             for j, assign in enumerate(assigns):
-                try:
-                    assign_href = assign.find_elements_by_tag_name(
-                        "a")[0].get_attribute("href")
-                    assignment_id = dict(
-                        parse.parse_qsl(
-                            parse.urlsplit(assign_href).query)).get('id', j)
-                    self.assiginments_buffer.append((lec_id, assign_href))
-                except Exception:
-                    print("COUDN'T GET ASSIGNMENT LINK")
+                self._extract_assignments(
+                    assign=assign, lec_id=lec_id, deufault_num=j)
+        return lectures
+
+    async def _run_assignments(self) -> Dict[int, crude_assignment]:
+        assignments: Dict[int, crude_assignment] = {}
 
         while (self.assiginments_buffer):
-            lec_id, assign_href = self.assiginments_buffer.pop()
+            lec_id, assignment_id, assign_href = self.assiginments_buffer.pop()
 
             # 課題情報ページに遷移
             self.driver.get(assign_href)
             time.sleep(5)
             print("current URL: {}".format(self.driver.current_url))
 
-            # コース名と課題名をゲットする
-            assign_title = self.driver.find_element(
-                By.TAG_NAME, "h2").text
-            print("課題名: %s" % assign_title)
-            WebDriverWait(self.driver, 10)
-            last_soup = BeautifulSoup(
-                self.driver.page_source, features="html.parser")
+            await self._insert_crude_assignment(assign_href, assignment_id, lec_id, assignments)
 
-            # 情報をゲットする
-            info = ''
-            try:
-                info = self.driver.find_element_by_css_selector(
-                    "td.cell,td.c1,td.lastcol").text
-            except Exception:
-                pass
-
-            # 課題の保存
-            try:
-                assignments[int(assignment_id)] = crude_assignment(
-                    course_id=int(lec_id),
-                    assignment_title=assign_title,
-                    info=info,
-                    url=assign_href,
-                )
-                print("AN ASSIGNMENT GOT")
-            except Exception:
-                print("AN ASSIGNMENT MISSED")
-
-        self.driver.quit()
-        return assignments, lectures
+        return assignments
 
 
 if __name__ == '__main__':
