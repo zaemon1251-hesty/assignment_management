790d56481c3345e2084f2c7653b0f391dce45a18
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-06-06 10:35:09 +0900

    chore: again*2 trying to deploy   render.com

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -1,5 +1,5 @@
 export const BACKEND_DOCKER_URL = 'http://web-server'
-export const BACKEND_LOCAL_URL = 'http://localhost:8888'
+export const BACKEND_LOCAL_URL = 'http://localhost:10000'
 
 export const ADMIN_USER =  {
     name: process.env.ADMIN_ACCOUNT,
